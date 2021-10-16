2c4c485f053bbd42c661254bb5e926a72fe3b7cb
Jesse Jaura <jessejaura@gmail.com>
2021-10-16 19:26:23 -0400

    Created framework to read and parse http request, and forward to correct http method

---
+++ Controller/HttpServerController.js
@@ -0,0 +1,15 @@
+function httpGet(httpMessage) {
+    return null;
+}
+
+function httpPost(httpMessage) {
+    return null;
+}
+
+function httpDelete(httpMessage) {
+    return null;
+}
+
+function httpPut(httpMessage) {
+    return null;
+}
