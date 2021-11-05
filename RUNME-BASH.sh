66e41615f5b065072e144d813bec6742a269fcf6
Jesse Jaura <jessejaura@gmail.com>
2021-11-05 17:56:59 -0400

    make scripts open browser

--- RUNME-BASH.sh
+++ RUNME-BASH.sh
@@ -8,3 +8,6 @@ docker rm http-server-container
 
 echo Running docker image
 docker run --detach -p 8080:8080 --name http-server-container http-server
+
+echo Opening http://localhost:8080/ on default browser
+python -m webbrowser http://localhost:8080/
--- RUNME-WINDOWS.cmd
+++ RUNME-WINDOWS.cmd
@@ -10,4 +10,7 @@ docker rm http-server-container
 ECHO Running docker container
 docker run --detach -p 8080:8080 --name http-server-container http-server
 
+ECHO Opening http://localhost:8080/ on default browser
+start http://localhost:8080/
+
 PAUSE
