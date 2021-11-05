1e4c1e7f81cf9a27e3006224729de2f764ab3e80
avocadoes <mg_zhu@outlook.com>
2021-11-05 19:24:20 -0400

    GET error tests and merge

--- Test/ControllerTests/HttpServerController.test.js
+++ Test/ControllerTests/HttpServerController.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const {HttpRequestMessage} = require('../../Models/HttpRequestMessage');
 const {httpGet} = require('../../Controller/HttpServerController.js');
+const {UnauthorizedRequestError, ForbiddenRequestError} = require('../../Models/Errors/HttpClientErrors');
 
 let request = new HttpRequestMessage('GET', '/one/two/three', 'HTTP/1.1', {}, '');
 let response =  'HTTP/1.1 200 OK\n'+
@@ -8,11 +9,19 @@ let response =  'HTTP/1.1 200 OK\n'+
                 'Content-Length: 44\n'+
                 'Connection: close\n\n'+
                 'GET request received on path: /one/two/three'
+let unauthorized = new HttpRequestMessage('GET', 'unauthorized', 'HTTP/1.1', {}, '');
+let forbidden = new HttpRequestMessage('GET', 'forbidden', 'HTTP/1.1', {}, '');
 
 describe('HTTP controller tests', () => {
     describe('GET', () => {
-        it('should return an HTTP GET response', () => {
+        it('should return an HTTP GET response on successful request', () => {
             assert.strictEqual(httpGet(request), response);
         });
-    })
+        it('should throw UnauthorizedRequestError on unauthorized request', () => {
+            assert.throws(() => httpGet(unauthorized), UnauthorizedRequestError);
+        });
+        it('should throw ForbiddenRequestError on forbidden request', () => {
+            assert.throws(() => httpGet(forbidden), ForbiddenRequestError);
+        });
+    });
 });
