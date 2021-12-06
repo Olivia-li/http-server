89dfff4a20b9cb4311a612d6c72e25450b7598e8
avocadoes <mg_zhu@outlook.com>
2021-12-05 20:19:16 -0500

    tests for GET

--- Test/ControllerTests/HttpServerController.test.js
+++ Test/ControllerTests/HttpServerController.test.js
@@ -4,25 +4,46 @@ const {httpGet} = require('../../Controller/HttpServerController.js');
 const {RequestURITooLongError, ForbiddenRequestError, NotFoundError} = require('../../Models/Errors/HttpClientErrors');
 const {SocketMock} = require('../Mocks/SocketMock');
 
-let long = new HttpRequestMessage('GET', '1'.repeat(256), 'HTTP/1.1', {}, '');
+let long = new HttpRequestMessage('GET', '/'+'1'.repeat(2048), 'HTTP/1.1', {}, '');
 let forbidden = new HttpRequestMessage('GET', '..', 'HTTP/1.1', {}, '');
 let fake = new HttpRequestMessage('GET', 'path', 'HTTP/1.1', {}, '');
-let test = new HttpRequestMessage('GET', '', {}, '');
-let socket = new SocketMock();
+let root = new HttpRequestMessage('GET', '/', {}, '');
+let folder = new HttpRequestMessage('GET', '/New%20folder', {}, '');
+let file = new HttpRequestMessage('GET', '/New%20folder/sample.html', {}, '');
 
 describe('HTTP controller tests', () => {
-    describe('GET', () => {
-        it('should throw RequestURITooLongError on target longer than 2048 characters total or with a filename longer than file system limits', () => {
-            assert.throws(() => httpGet(long, null), RequestURITooLongError);
+    describe('httpGET', () => {
+        it('should throw RequestURITooLongError on path longer than 2048 characters', () => {
+            let socket = new SocketMock();
+            assert.throws(() => httpGet(long, socket), RequestURITooLongError);
+            
         });
         it('should throw ForbiddenRequestError on parent directory target', () => {
-            assert.throws(() => httpGet(forbidden, null), ForbiddenRequestError);
+            let socket = new SocketMock();
+            assert.throws(() => httpGet(forbidden, socket), ForbiddenRequestError);
+            
         });
         it('should throw NotFoundError on target not found', () => {
-            assert.throws(() => httpGet(fake, null), NotFoundError);
+            let socket = new SocketMock();
+            assert.throws(() => httpGet(fake, socket), NotFoundError);
         });
-        it('should write to socket and return nothing on success', () => {
-            assert.strictEqual(httpGet(test, socket), undefined);
-        });        
+        it('should write root folder to socket and return nothing on success', () => {
+            let socket = new SocketMock();
+            assert.strictEqual(socket.written, false);
+            assert.strictEqual(httpGet(root, socket), undefined);
+            assert.strictEqual(socket.written, true);
+        });
+        it('should write subfolder to socket and return nothing on success', () => {
+            let socket = new SocketMock();
+            assert.strictEqual(socket.written, false);
+            assert.strictEqual(httpGet(folder, socket), undefined);
+            assert.strictEqual(socket.written, true);
+        });
+        it('should write file to socket and return nothing on success', () => {
+            let socket = new SocketMock();
+            assert.strictEqual(socket.written, false);
+            assert.strictEqual(httpGet(file, socket), undefined);
+            assert.strictEqual(socket.written, true);
+        });     
     });
 });
